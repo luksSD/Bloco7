d34b5d90d50f7a88be07b2d441a3a5476787f267
Lucas Dias <lucas.sd94@gmail.com>
2021-09-26 15:58:53 -0300

    API-CONTROLLER implementado

---
+++ bloco7-projeto/bloco7-api/src/main/java/br/fai/bloco7/api/controller/PessoaRestController.java
@@ -0,0 +1,57 @@
+package br.fai.bloco7.api.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import br.fai.bloco7.api.service.PessoaService;
+import br.fai.bloco7.model.Pessoa;
+
+public class PessoaRestController {
+
+	@Autowired
+	private PessoaService service;
+
+	@GetMapping("/read-all")
+	public ResponseEntity<List<Pessoa>> readAll() {
+
+		return ResponseEntity.ok(service.readAll());
+
+	}
+
+	@GetMapping("/read-by-id/{id}")
+	public ResponseEntity<Pessoa> readById(@PathVariable("id") final long id) {
+
+		return ResponseEntity.ok(service.readById(id));
+
+	}
+
+	@PostMapping("/create")
+	public ResponseEntity<Long> create(@RequestBody final Pessoa pessoa) {
+
+		return ResponseEntity.ok(service.create(pessoa));
+
+	}
+
+	@PutMapping("/update")
+	public ResponseEntity<Boolean> update(@RequestBody final Pessoa pessoa) {
+
+		return ResponseEntity.ok(service.update(pessoa));
+
+	}
+
+	@DeleteMapping("/delete/{id}")
+	public ResponseEntity<Boolean> delete(@PathVariable("id") final long id) {
+
+		return ResponseEntity.ok(service.delete(id));
+
+	}
+
+}
