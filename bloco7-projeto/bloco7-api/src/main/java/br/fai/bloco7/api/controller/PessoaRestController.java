6d931bb502b7b044935671946f9730df160d4749
Lucas Dias <lucas.sd94@gmail.com>
2021-09-26 16:01:00 -0300

    API-CONTROLLER atualizado

--- bloco7-projeto/bloco7-api/src/main/java/br/fai/bloco7/api/controller/PessoaRestController.java
+++ bloco7-projeto/bloco7-api/src/main/java/br/fai/bloco7/api/controller/PessoaRestController.java
@@ -4,16 +4,22 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import br.fai.bloco7.api.service.PessoaService;
 import br.fai.bloco7.model.Pessoa;
 
+@RestController
+@RequestMapping("/api/v1/pessoa")
+@CrossOrigin(origins = "*")
 public class PessoaRestController {
 
 	@Autowired
