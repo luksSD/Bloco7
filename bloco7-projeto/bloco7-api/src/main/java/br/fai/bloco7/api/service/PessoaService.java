076ea70f736b5c9d27e5373d542193189c5de71b
Lucas Dias <lucas.sd94@gmail.com>
2021-09-26 15:54:31 -0300

    API-SERVICE IMPLEMENTADO

---
+++ bloco7-projeto/bloco7-api/src/main/java/br/fai/bloco7/api/service/PessoaService.java
@@ -0,0 +1,19 @@
+package br.fai.bloco7.api.service;
+
+import java.util.List;
+
+import br.fai.bloco7.model.Pessoa;
+
+public interface PessoaService {
+
+	List<Pessoa> readAll();
+
+	Pessoa readById(Long id);
+
+	Long create(Pessoa entity);
+
+	boolean update(Pessoa entity);
+
+	boolean delete(Long id);
+
+}
