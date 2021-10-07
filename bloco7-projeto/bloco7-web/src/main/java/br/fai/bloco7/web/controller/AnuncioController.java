76c15f0b79aa6272d24e2a1db04d7ddbf136b0b2
Lucas Dias <lucas.sd94@gmail.com>
2021-10-06 21:31:14 -0300

    Single Page de Anuncio criada

--- bloco7-projeto/bloco7-web/src/main/java/br/fai/bloco7/web/controller/AnuncioController.java
+++ bloco7-projeto/bloco7-web/src/main/java/br/fai/bloco7/web/controller/AnuncioController.java
@@ -2,13 +2,20 @@ package br.fai.bloco7.web.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/anuncios")
 public class AnuncioController {
 
-	@GetMapping("/anuncios")
+	@GetMapping("/listar")
 	public String getAnuncioGridPage() {
 		return "anuncio/anuncios-grid";
+	}
+
+	@GetMapping("/1")
+	public String getSinglePageAnuncio() {
+		return "anuncio/anuncio-single";
 	}
 
 }
