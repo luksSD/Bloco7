6b79bbbd18db1e16e79005ead6ac2bd9c3a31487
Lucas Dias <lucas.sd94@gmail.com>
2021-10-14 19:52:49 -0300

    novas atualizações

--- bloco7-projeto/bloco7-db/src/main/java/br/fai/bloco7/db/dao/CidadeDao.java
+++
@@ -1,9 +0,0 @@
-package br.fai.bloco7.db.dao;
-
-import br.fai.bloco7.model.Cidade;
-
-public interface CidadeDao {
-
-	Cidade readById(Long id);
-
-}
--- bloco7-projeto/bloco7-web/src/main/java/br/fai/bloco7/web/service/CidadeService.java
+++
@@ -1,9 +0,0 @@
-package br.fai.bloco7.web.service;
-
-import br.fai.bloco7.model.Cidade;
-
-public interface CidadeService {
-
-	Cidade readById(Long id);
-
-}
