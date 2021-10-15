130ea769e9bc82191487f770b1e6b03f561ad985
Vinicius Almeida <viniciusdealmeida@hotmail.com>
2021-10-15 20:32:37 -0300

    Alteração no B.D

--- Bd/bloco7.sql
+++ Bd/bloco7.sql
@@ -54,7 +54,7 @@ alter table pessoa add constraint fk2_pessoa foreign key (usuario_id) references
 CREATE TABLE anuncio
 (
     id BIGSERIAL primary key,
-    descricao VARCHAR (50) not null,
+    descricao VARCHAR (1000) not null,
     quartos integer not null,
     banheiros integer not null,
     vaga_garagem integer,
@@ -143,37 +143,41 @@ values ('4' , 'Minas Gerais' , 'Santa Rita do Sapucai');
 --Adicionando valores para teste
 
 INSERT INTO usuario (id , senha, email, celular)
- values ('1' , '123' , 'teste@gmail.com' , '999999999');
+ values ('1' , '123' , 'vinicius@gmail.com' , '(35)99757-8408');
 
 INSERT INTO pessoa (usuario_id , cpf, nome, logradouro, numero, bairro, cep, cidade_id ) 
-values ('1' , '123.456.789.11' , 'Vinicius Teste' , 'Rua Francisco' , '27' , 'Centro' , '37550000' , '1');
+values ('1' , '123.456.789.11' , 'Vinicius de Almeida Gonçalves' , 'Rua Lourdes de Souza Santos' , '90' , 'Coline Verde' , '37550000' , '1');
 
 
 INSERT INTO usuario (id , senha, email, celular)
- values ('2' , '123' , 'teste02@hotmail.com' , '333333333333');
+ values ('2' , '123' , 'lucas@gmail.com' , '(35)99873-8536');
 
 INSERT INTO pessoa (usuario_id , cpf, nome, logradouro, numero, bairro, cep, cidade_id )
-values ('2' , '111.111.111.11' , 'Lucas Teste' , 'Rua Joao' , '46' , 'Fatima' , '37550000' , '2');
+values ('2' , '123.456.789.12' , 'Lucas Silva e Dias' , 'Avenida Barão do Rio Branco' , '46' , 'Centro' , '37540-000' , '4');
 
 
 INSERT INTO usuario (id , senha, email, celular)
- values ('3' , '123' , 'teste03@hotmail.com' , '444444444');
+ values ('3' , '123' , 'matheus@gmail.com' , '(35)99114-3152');
 
  INSERT INTO pessoa (usuario_id , cpf, nome, logradouro, numero, bairro, cep, cidade_id )
-values ('3' , '222.222.222.22' , 'Matheus Teste' , 'Rua Roberto' , '46' , 'Bento' , '37550000' , '3');
+values ('3' , '123.456.789.13' , 'Matheus Felipe de Souza' , 'Rua Crescêncio Ribeiro' , '14' , 'Fernandes' , '37540-000' , '4');
 
 
  INSERT INTO anuncio
  (id , descricao, quartos, banheiros, vaga_garagem , tipo_propriedade , status, area, preco , endereco, bairro, cep, cidade_id, usuario_anunciante_id)
- values ('1' , 'Linda casa em bairro familiar' , '4' , '2' , '2' , 'Casa' , 'Alugar' , '1000', '2000' ,'Rua Francisco Nº 30' , 'Centro' , '37550-000' , '1' , '1');
+ values ('1' , 'Apartamento em frente a receita federal. Região central. Rápido acesso a bancos e lotéricas. Condomínio com segurança. Sala ampla, cozinha com armários. Possui elevador' 
+    , '4' , '2' , '1' , 'Apartamento' , 'Aluguel' , '100', '870' ,'Rua Francisco Nº 30' , 'Centro' , '37550-000' , '1' , '1');
 
 
 INSERT INTO anuncio
  (id , descricao, quartos, banheiros, vaga_garagem , tipo_propriedade , status, area, preco , endereco, bairro, cep, cidade_id, usuario_anunciante_id)
- values ('2' , 'Ap. para solteiro' , '1' , '1' , '0' , 'Apartamento' , 'Alugar' , '100', '500', 'Rua João Nº 60' , 'Fatima' , '37550-000' , '2' , '2');
+ values ('2' , 'Casa sede em madeira com 140m a/c, 2 amplos dormitórios, sala em 2 ambientes, ampla cozinha e despensa. Na parte externa, piscina 4x8 com deck em pedra mineira, jardim, espaço gourmet completo, churrasqueira, campo de futebol. Linda Vista para desfrutar do que a natureza tem de melhor!!!
+
+' , '2' , '3' , '2' , 'Chácara' , 'Venda' , '1000', '500000', 'Rua Costinha Nº 79' , 'Zona Rural' , '37540-000' , '4' , '2');
 
  INSERT INTO anuncio
  (id , descricao, quartos, banheiros, vaga_garagem , tipo_propriedade , status, area, preco , endereco, bairro, cep, cidade_id, usuario_anunciante_id)
- values ('3' , 'Alugo casa que aceita animais' , '2' , '1' , '1' , 'Casa' , 'Alugar' , '500', '1500' ,'Rua Roberto Nº 145' , 'Bento' , '37550-000' , '3' , '3');
+ values ('3' , 'Maravilhosa casa, com um excelente construção, isso sem falar da área de laser..., casa em 2 pavimentos com uma escada bem generosa, ampla, clara e muito arejada, no primeiro pavimento com um amplo jardim na frente da casa, varandão e as vagas de garagem, existe um Soto em baixo da casa, piscina, área livre, uma academia, sauna, bar gourmet, sala de tv, quarto e um banheiro , essa é a parte de fora da casa, ainda no primeiro pavimento, salão em L quarto que é uma suíte, copa cozinha, lavabo, dispensa e lavanderia, casa com energia solar. Segundo pavimento- 3 quartos, todos com varanda, uma suíte e um banheiro social. Cômodos todos com armários.' 
+    , '4' , '3' , '2' , 'Casa' , 'Aluguel' , '400', '7000' ,'Rua Roberto Nº 145' , 'Jardim Paraiso' , '37550-000' , '4' , '3');
 
 
